1611302026fa4480f8fd82a1a6551d6cea4fd5ab
William Showalter <williamshowalter@gmail.com>
2013-04-30 20:57:24 -0800

    Initial Commit

---
+++ WilhelmSCP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WilhelmSCP
+//
+//  Created by William Showalter on 4/30/13.
+//  Copyright (c) 2013 William Showalter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
